3d6ad577a1e3bbe50169b9caf04590fa18f0e42f
CarlosGutz <carlosgutz25@gmail.com>
2018-02-26 11:22:09 -0800

    Added unit tests via Mocha Chai but modularization was not heavily used

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,8 @@ import App from './App';
 import chai from 'chai';
 import {expect} from 'chai';
 import chaiImmutable from 'chai-immutable';
-import {List, Map} from 'immutable';
+import {List, Map, fromJS} from 'immutable';
+import {setEntries, next, vote} from './core';
 
 
 it('renders without crashing', () => {
@@ -86,4 +87,259 @@ describe('immutability', () => {
             }));
         });
     });
+
+    describe('next', () => {
+
+        it('takes the next two entries under vote', () => {
+            const state = Map({
+                entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later')
+                }),
+                entries: List.of('Sunshine')
+            }));
+        });
+
+    });
+});
+
+describe('application logic', () => {
+
+    describe('setEntries', () => {
+
+        it('adds the entries to the state', () => {
+            const state = Map();
+            const entries = List.of('Trainspotting', '28 Days Later');
+            const nextState = setEntries(state, entries);
+            expect(nextState).to.equal(Map({
+                entries: List.of('Trainspotting', '28 Days Later')
+            }));
+        });
+
+    });
+
+});
+
+
+describe('application logic', () => {
+
+    describe('setEntries', () => {
+
+        it('adds the entries to the state', () => {
+            const state = Map();
+            const entries = List.of('Trainspotting', '28 Days Later');
+            const nextState = setEntries(state, entries);
+            expect(nextState).to.equal(Map({
+                entries: List.of('Trainspotting', '28 Days Later')
+            }));
+        });
+
+    });
+
+});
+
+describe('application logic', () => {
+
+    describe('vote', () => {
+
+        it('creates a tally for the voted entry', () => {
+            const state = Map({
+                pair: List.of('Trainspotting', '28 Days Later')
+            });
+            const nextState = vote(state, 'Trainspotting')
+            expect(nextState).to.equal(Map({
+                pair: List.of('Trainspotting', '28 Days Later'),
+                tally: Map({
+                    'Trainspotting': 1
+                })
+            }));
+        });
+
+        it('adds to existing tally for the voted entry', () => {
+            const state = Map({
+                pair: List.of('Trainspotting', '28 Days Later'),
+                tally: Map({
+                    'Trainspotting': 3,
+                    '28 Days Later': 2
+                })
+            });
+            const nextState = vote(state, 'Trainspotting');
+            expect(nextState).to.equal(Map({
+                pair: List.of('Trainspotting', '28 Days Later'),
+                tally: Map({
+                    'Trainspotting': 4,
+                    '28 Days Later': 2
+                })
+            }));
+        });
+
+    });
+
+    describe('next', () => {
+
+        it('puts winner of current vote back to entries', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later'),
+                    tally: Map({
+                        'Trainspotting': 4,
+                        '28 Days Later': 2
+                    })
+                }),
+                entries: List.of('Sunshine', 'Millions', '127 Hours')
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Sunshine', 'Millions')
+                }),
+                entries: List.of('127 Hours', 'Trainspotting')
+            }));
+        });
+
+        it('puts both from tied vote back to entries', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later'),
+                    tally: Map({
+                        'Trainspotting': 3,
+                        '28 Days Later': 3
+                    })
+                }),
+                entries: List.of('Sunshine', 'Millions', '127 Hours')
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Sunshine', 'Millions')
+                }),
+                entries: List.of('127 Hours', 'Trainspotting', '28 Days Later')
+            }));
+        });
+
+        it('marks winner when just one entry left', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later'),
+                    tally: Map({
+                        'Trainspotting': 4,
+                        '28 Days Later': 2
+                    })
+                }),
+                entries: List()
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                winner: 'Trainspotting'
+            }));
+        });
+
+    });
+
+});
+
+it('converts to immutable', () => {
+    const state = Map();
+    const entries = ['Trainspotting', '28 Days Later'];
+    const nextState = setEntries(state, entries);
+    expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+    }));
+});
+
+
+import reducer from './reducers';
+
+describe('reducer', () => {
+
+    it('has an initial state', () => {
+        const action = {type: 'SET_ENTRIES', entries: ['Trainspotting']};
+        const nextState = reducer(undefined, action);
+        expect(nextState).to.equal(fromJS({
+            entries: ['Trainspotting']
+        }));
+    });
+
+    it('handles SET_ENTRIES', () => {
+        const initialState = Map();
+        const action = {type: 'SET_ENTRIES', entries: ['Trainspotting']};
+        const nextState = reducer(initialState, action);
+
+        expect(nextState).to.equal(fromJS({
+            entries: ['Trainspotting']
+        }));
+    });
+
+    it('handles NEXT', () => {
+        const initialState = fromJS({
+            entries: ['Trainspotting', '28 Days Later']
+        });
+        const action = {type: 'NEXT'};
+        const nextState = reducer(initialState, action);
+
+        expect(nextState).to.equal(fromJS({
+            vote: {
+                pair: ['Trainspotting', '28 Days Later']
+            },
+            entries: []
+        }));
+    });
+
+    it('handles VOTE', () => {
+        const initialState = fromJS({
+            vote: {
+                pair: ['Trainspotting', '28 Days Later']
+            },
+            entries: []
+        });
+        const action = {type: 'VOTE', entry: 'Trainspotting'};
+        const nextState = reducer(initialState, action);
+
+        expect(nextState).to.equal(fromJS({
+            vote: {
+                pair: ['Trainspotting', '28 Days Later'],
+                tally: {Trainspotting: 1}
+            },
+            entries: []
+        }));
+    });
+
+});
+
+it('can be used with reduce', () => {
+    const actions = [
+        {type: 'SET_ENTRIES', entries: ['Trainspotting', '28 Days Later']},
+        {type: 'NEXT'},
+        {type: 'VOTE', entry: 'Trainspotting'},
+        {type: 'VOTE', entry: '28 Days Later'},
+        {type: 'VOTE', entry: 'Trainspotting'},
+        {type: 'NEXT'}
+    ];
+    const finalState = actions.reduce(reducer, Map());
+
+    expect(finalState).to.equal(fromJS({
+        winner: 'Trainspotting'
+    }));
+});
+
+import makeStore from './store';
+
+describe('store', () => {
+
+    it('is a Redux store configured with the correct reducer', () => {
+        const store = makeStore();
+        expect(store.getState()).to.equal(Map());
+
+        store.dispatch({
+            type: 'SET_ENTRIES',
+            entries: ['Trainspotting', '28 Days Later']
+        });
+        expect(store.getState()).to.equal(fromJS({
+            entries: ['Trainspotting', '28 Days Later']
+        }));
+    });
+
 });
